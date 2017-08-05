7a932971e0833725aa351281fecd1cb9de287aee
Jie Liang <jieliang456@gmail.com>
2017-08-04 23:41:29 -0400

    basic responsive navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, OnInit, HostListener } from '@angular/core';
+import { MdSidenav } from "@angular/material";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  @ViewChild('sidenav') sidenav: MdSidenav;
+
+  constructor(
+  ) { }
+
+  ngOnInit() {
+    if (window.innerWidth < 768) {
+      this.sidenav.close();
+    }
+  }
+
+  @HostListener('window:resize', ['$event'])
+    onResize(event) {
+        if (event.target.innerWidth < 768) {
+            this.sidenav.close();
+        }
+        if (event.target.innerWidth == 768) {
+            this.sidenav.close();
+        }
+        if (event.target.innerWidth > 768) {
+           this.sidenav.open();
+        }
+    }
 }
