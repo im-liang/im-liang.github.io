8b301fd0cc563b524d7c0816fbea72d8cd8e2790
Jie Liang <jieliang456@gmail.com>
2017-11-27 12:01:39 -0500

    ngFor to create list of projects

--- src/app/project/project.component.ts
+++ src/app/project/project.component.ts
@@ -1,11 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 
+export class Project {
+  name: string;
+  link: string;
+  description: string;
+}
+const PROJECTS: Project[] = [
+  { name: 'Mr. Nice', link: '#', description: 'Zoe never had the patience of her sisters. She deliberately punched the bear in his face.' },
+  { name: 'Narco', link: '#', description: 'Zoe never had the patience of her sisters. She deliberately punched the bear in his face.' },
+  { name: 'Bombasto', link: '#', description: 'Zoe never had the patience of her sisters. She deliberately punched the bear in his face.' },
+  { name: 'Celeritas', link: '#', description: 'Zoe never had the patience of her sisters. She deliberately punched the bear in his face.' },
+  { name: 'Magneta', link: '#', description: 'Zoe never had the patience of her sisters. She deliberately punched the bear in his face.' },
+  { name: 'RubberMan', link: '#', description: 'Zoe never had the patience of her sisters. She deliberately punched the bear in his face.' },
+  { name: 'Dynama', link: '#', description: 'Zoe never had the patience of her sisters. She deliberately punched the bear in his face.' },
+  { name: 'Dr IQ', link: '#', description: 'Zoe never had the patience of her sisters. She deliberately punched the bear in his face.' },
+  { name: 'Magma', link: '#', description: 'Zoe never had the patience of her sisters. She deliberately punched the bear in his face.' },
+  { name: 'Tornado', link: '#', description: 'Zoe never had the patience of her sisters. She deliberately punched the bear in his face.' }
+];
+
 @Component({
   selector: 'app-project',
   templateUrl: './project.component.html',
   styleUrls: ['./project.component.css']
 })
 export class ProjectComponent implements OnInit {
+
+  projects = PROJECTS;
 
   constructor() { }
 
