eefbf5802a6acd76d1890b39e19bdcd8719eb348
Jie Liang <jieliang456@gmail.com>
2017-12-15 14:20:49 -0500

    add scrolling effect on mouse icon of blog page

--- src/app/blog/blog.component.ts
+++ src/app/blog/blog.component.ts
@@ -21,4 +21,17 @@ export class BlogComponent implements OnInit {
       .subscribe(blogs => this.blogs = blogs);
   }
 
+  jumpToId( fragment ): void {
+
+    // Use the browser to navigate
+    window.location.hash = fragment;
+
+    // But also scroll when routing / deep-linking to dynamic page
+    // or re-clicking same anchor
+    if (fragment) {
+        const element = document.querySelector('#' + fragment);
+        if (element) element.scrollIntoView({behavior: 'smooth'});
+    }
+}
+
 }
--- src/app/data/blog-data.ts
+++ src/app/data/blog-data.ts
@@ -13,6 +13,39 @@ export const BLOG: Blog[] = [
     tags: ['Python3', 'Docker']
   },
   {
+    file: '/assets/blogs/test.md',
+    title: 'Stranger Things: The sound of the Upside Down',
+    description: 'The antsy bingers of Netflix will eagerly anticipate the digital release of the Survive soundtrack, out today.',
+    link: 'Aug-12-2017-test',
+    cover: '/assets/x.jpg',
+    day: 12,
+    month: 'Aug',
+    year: 2017,
+    tags: ['Python3', 'Docker']
+  },
+  {
+    file: '/assets/blogs/test.md',
+    title: 'Stranger Things: The sound of the Upside Down',
+    description: 'The antsy bingers of Netflix will eagerly anticipate the digital release of the Survive soundtrack, out today.',
+    link: 'Aug-12-2017-test',
+    cover: '/assets/x.jpg',
+    day: 12,
+    month: 'Aug',
+    year: 2017,
+    tags: ['Python3', 'Docker']
+  },
+  {
+    file: '/assets/blogs/test.md',
+    title: 'Stranger Things: The sound of the Upside Down',
+    description: 'The antsy bingers of Netflix will eagerly anticipate the digital release of the Survive soundtrack, out today.',
+    link: 'Aug-12-2017-test',
+    cover: '/assets/x.jpg',
+    day: 12,
+    month: 'Aug',
+    year: 2017,
+    tags: ['Python3', 'Docker']
+  },
+  {
     file: '/assets/blogs/python_docker.md',
     title: 'Stranger Things: The sound of the Upside Down',
     description: 'The antsy bingers of Netflix will eagerly anticipate the digital release of the Survive soundtrack, out today.',
